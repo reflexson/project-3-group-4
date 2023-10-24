d1b8549058c948c273998d39eaf6bf1073986c0f
Camille MacLaren <camillemaclaren@gmail.com>
2023-10-24 14:30:32 -0500

    Update Signup.js
    add re-enter password
    logic to check if passwords are matching
    add the html

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -6,24 +6,30 @@ import Auth from '../utils/auth';
 
 function Signup(props){
     // state
-    const [formState, setFormState] = useState({ username: '', password: '' });
+    const [formState, setFormState] = useState({ username: '', password: '', rePassword: '' });
     const [createUser] = useMutation(CREATE_USER);
-
+    let notSame = null;
     // form handler
     const handleFormSubmit = async (event) => {
         // prevents form sumbitting to itself
         event.preventDefault();
-        // create user
-        const mutationResponse = await CREATE_USER({
-            variables: {
-                username: formState.username,
-                password: formState.password,
-            }
-        });
-        // make token for session
-        const token = mutationResponse.data.addUser.token;
-        // validate token
-        Auth.login(token);
+        // check if password and repassword are the same
+        if(formState.password !== formState.rePassword){
+            notSame = 'not the same'
+        } else {
+            // create user
+            const mutationResponse = await CREATE_USER({
+                variables: {
+                    username: formState.username,
+                    password: formState.password,
+                }
+            });
+            // make token for session
+            const token = mutationResponse.data.addUser.token;
+            // validate token
+            Auth.login(token);
+        }
+        
     };
 
     // detect change in the form
@@ -34,6 +40,58 @@ function Signup(props){
           [name]: value,
         });
     };
+
+    // the html
+    return(
+        <div className="container">
+            {/* Redirect to signup */}
+            <Link to="/login">← Go to Login</Link>
+            
+            <h2>Signup</h2>
+            {/* Form */}
+            <form onSubmit={handleFormSubmit}>
+                <div className='form-item username'>
+                    <label htmlFor='username'>Username:</label>
+                    <input
+                    placeholder='username'
+                    name='username'
+                    type='text'
+                    id='username'
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className='form-item password'>
+                    <label htmlFor='password'>Password:</label>
+                    <input
+                    placeholder='password'
+                    name='password'
+                    type='password'
+                    id='password'
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className='form-item password rePassword'>
+                    <label htmlFor='rePassword'>Re-enter Password:</label>
+                    <input
+                    placeholder='password'
+                    name='rePassword'
+                    type='password'
+                    id='rePassword'
+                    onChange={handleChange}
+                    />
+                </div>
+                {/* If error, show error message*/}
+                {notSame ? (
+                    <div>
+                        <p className='error-text'>Error: Passwords must be matching</p>
+                    </div>
+                ) : null}
+                {/* submit button */}
+                <button className='btn' type='submit'>Submit</button>
+            </form>
+
+        </div>
+    );
 }
 
 export default Signup;
