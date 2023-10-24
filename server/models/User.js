07dbc5b6d5cc24444692f9b5b77f0d42f95ebabb
Jacob Pudwill <jacobpudwill@gmail.com>
2023-10-23 20:19:48 -0500

    Set up bcrypt.

--- server/models/User.js
+++ server/models/User.js
@@ -13,6 +13,21 @@ const userSchema = new Schema({
     },
 });
 
+// Set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// Compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  await bcrypt.compare(password, this.password);
+};
+
 const User = model('User', userSchema);
 
 module.exports = User;
