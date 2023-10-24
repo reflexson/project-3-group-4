dd81214a3762f9024d412a53f88be0100e9ed8cf
Jacob Pudwill <jacobpudwill@gmail.com>
2023-10-23 19:26:06 -0500

    Set up basic user model.

---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
