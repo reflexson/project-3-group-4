39c2c90d6c3a04636938ba28e684953e20b233f4
reflexson <131407662+reflexson@users.noreply.github.com>
2023-10-24 12:29:56 -0500

    css bootstrap homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 import {
   ApolloClient,
