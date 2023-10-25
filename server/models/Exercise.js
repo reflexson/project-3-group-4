93cbb9396cab24b666c179f662297d0d636f4f92
Jacob Pudwill <jacobpudwill@gmail.com>
2023-10-25 17:45:44 -0500

    Set up set model.

--- server/models/Exercise.js
+++ server/models/Exercise.js
@@ -1,10 +1,12 @@
 const { Schema, model } = require('mongoose');
+const Set = require('./Set');
 
 const exerciseSchema = new Schema({
     exercise: {
         type: String,
         required: true
-    }
+    },
+    sets: [Set.schema]
 });
 
 const Exercise = model('Exercise', exerciseSchema);
