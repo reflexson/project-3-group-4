fed45625bc3b2732a3783bebcbafe79dfd188a3d
abstractl0gic <zwlyons@gmail.com>
2023-10-25 19:54:46 -0500

    add mutation to resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,56 +1,15 @@
 const { AuthenticationError } = require('apollo-server-express');
-<<<<<<< HEAD
-const { User, Workout, Set } = require('../models');
-const { signToken } = require('../utils/auth');
-
-const resolvers = {
-  Query: {
-    getUser: async (_, { id }, context) => {
-      if (context.user) {
-        return await User.findById(id);
-      }
-      throw new AuthenticationError('Not logged in');
-    },
-    getWorkout: async (_, { id }, context) => {
-      if (context.user) {
-        return await Workout.findById(id).populate('sets');
-      }
-      throw new AuthenticationError('Not logged in');
-    },
-  },
-  Mutation: {
-    createUser: async (_, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-      return { token, user };
-    },
-    createWorkout: async (_, { userId, date, sets }, context) => {
-      if (context.user) {
-        const workout = await Workout.create({ userId, date, sets });
-        return await Workout.findById(workout._id).populate('sets');
-      }
-      throw new AuthenticationError('Not logged in');
-    },
-    createSet: async (_, { exercise, repetitions, weight, duration, notes }) => {
-      return await Set.create({ exercise, repetitions, weight, duration, notes });
-    },
-  },
-};
-
-module.exports = resolvers;
-=======
-const { User, Workout, Exercise, Set } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Mutation: {
-    addUser: async (parent, args) => {
+    addUser: async (_, args) => {
       const user = await User.create(args);
       const token = signToken(user);
-
       return { token, user };
     },
-    login: async (parent, { username, password }) => {
+    login: async (_, { username, password }) => {
       const user = await User.findOne({ username });
 
       if (!user) {
@@ -64,11 +23,9 @@ const resolvers = {
       }
 
       const token = signToken(user);
-
       return { token, user };
-    }
-  }
+    },
+  },
 };
 
 module.exports = resolvers;
->>>>>>> 3f40ac9381a5439445ba0c718233a2bc1f490fdd
