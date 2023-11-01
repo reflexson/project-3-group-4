59740d8a4b287e60772f5260a4130cf35a114a55
abstractl0gic <zwlyons@gmail.com>
2023-11-01 13:42:12 -0500

    update pageload logic

--- client/src/pages/Progress.js
+++ client/src/pages/Progress.js
@@ -1,62 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Bar, Line } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
 import Select from "react-select";
-import {GET_SETS} from "../utils/queries";
+import { GET_SETS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 import { useSettingsContext } from "../utils/GlobalState";
 import { convertImperialToMetric } from "../utils/unitConversion";
 
 Chart.register(...registerables);
+
 const Progress = () => {
   const { loading, data } = useQuery(GET_SETS);
   const sets = data?.sets || [];
   const [selectedOption, setSelectedOption] = useState("Week");
+  const [chartData, setChartData] = useState(null); // Initialize chartData state
 
-  let [ settingsState, setSettingsState] = useSettingsContext();
-  const [weightLabel, setweightLabel]= useState('lbs');
-    if(settingsState.units === 'metric' && weightLabel === 'lbs'){
-        setweightLabel('kg');
+  let [settingsState, setSettingsState] = useSettingsContext();
+  const [weightLabel, setweightLabel] = useState("lbs");
+  if (settingsState.units === "metric" && weightLabel === "lbs") {
+    setweightLabel("kg");
   }
-  // for (let i = 0; i<sets.length; i++) {
-  //   let setEx = sets[i].exercise;
-  //   exArray.push({setEx});
- 
-  // }
-  // const exObArray = sets.map((ex) => (
-  //   {
-  //       value: ex.exercise,
-  //       label: ex.exercise
-  //   }
-  // ));
 
-  let exProgress = sets.filter(function(el){
-    return el.exercise === selectedOption
-  })
+  let exProgress = sets.filter(function (el) {
+    return el.exercise === selectedOption;
+  });
 
-  const exArray = sets.map((ex) => (ex.exercise));
+  const exArray = sets.map((ex) => ex.exercise);
 
-  var exArrayUnique = [...new Set(exArray)]
-  const exObArray = exArrayUnique.map((e) => (
-    {
-        value: e,
-        label: e
-    }
-  ));
+  var exArrayUnique = [...new Set(exArray)];
+  const exObArray = exArrayUnique.map((e) => ({
+    value: e,
+    label: e,
+  }));
 
-  console.log('exProgress:', exProgress);
+  console.log("exProgress:", exProgress);
 
   let xAxis = exProgress.map((set) => set.date);
   let yAxis = exProgress.map((set) => set.oneRepMax);
   console.log(yAxis);
-  if(settingsState.units === 'metric'){
+  if (settingsState.units === "metric") {
     yAxis = yAxis.map((set) => convertImperialToMetric(set));
   }
 
   console.log(xAxis);
   console.log(yAxis);
- 
+
   const weekChartData = {
     labels: xAxis,
     datasets: [
@@ -113,16 +102,19 @@ const Progress = () => {
   };
   const dropdownOptions = exObArray;
 
-  const [chartData, setChartData] = useState(weekChartData);
-  const handleChange = (selectedOptions) => {
-    setSelectedOption(selectedOptions.value);
-    if (selectedOptions.value === "Excercise") {
+  useEffect(() => {
+    // Set initial chartData based on selectedOption
+    if (selectedOption === "Excercise") {
       setChartData(excerciseChartData);
     } else {
       setChartData(weekChartData);
     }
-    console.log(selectedOption);
+  }, [selectedOption]); // Trigger this effect when selectedOption changes
+
+  const handleChange = (selectedOptions) => {
+    setSelectedOption(selectedOptions.value);
   };
+
   return (
     <div className="col-12 flex-row">
       <aside className="w3-sidebar  w3-bar-block">
@@ -139,21 +131,20 @@ const Progress = () => {
       </aside>
 
       <main className="dashcont">
-        <div  style={{ width: "35%", marginLeft: "25%", padding: "2%" }}>
+        <div style={{ width: "35%", marginLeft: "25%", padding: "2%" }}>
           <Select
             options={dropdownOptions}
             default={selectedOption}
             onChange={handleChange}
           />
         </div>
-        {/* <div className="graph"style={{ width: "35%", marginLeft: "25%", padding: "2%" }}>
-          <Bar data={chartData} options={options} />
-        </div> */}
-        <div className="graph"style={{ width: "75%", marginLeft: "10%", padding: "2%" }}>
-          <Line data={chartData} options={options} />
+        <div
+          className="graph"
+          style={{ width: "75%", marginLeft: "10%", padding: "2%" }}
+        >
+          {chartData && <Line data={chartData} options={options} />}
         </div>
       </main>
-      
     </div>
   );
 };
